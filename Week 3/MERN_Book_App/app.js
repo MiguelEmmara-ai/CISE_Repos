0262343fa961c749d6547b610b296f88212b81c7
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-07-30 02:02:21 +1200

    Building RESTful APIs with the MERN stack

--- Week 3/MERN_Book_App/app.js
+++ Week 3/MERN_Book_App/app.js
@@ -3,12 +3,21 @@
 const express = require("express");
 const connectDB = require("./config/db");
 
+// routes
+const books = require("./routes/api/books.js");
+
 const app = express();
 
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// use Routes
+app.use("/api/books", books);
 
 const port = process.env.PORT || 5123;
 
--- Week 3/MERN_Book_App/config/db.js
+++ Week 3/MERN_Book_App/config/db.js
@@ -1,13 +1,10 @@
-const { mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(db, { useNewUrlParser: true });
 
     console.log("MongoDB is Connected...");
   } catch (err) {
