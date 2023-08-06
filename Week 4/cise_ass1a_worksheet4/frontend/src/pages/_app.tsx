7a488048b3d85790bc5434f131b6bfa59e6f50c7
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-08-06 15:04:28 +1200

    Fix Typo

--- Week 4/cise_ass1a_worksheet4/frontend/src/pages/_app.tsx
+++ Week 4/cise_ass1a_worksheet4/frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import PopulatedNavBar from "../components/PopulatedNavbar";
+import PopulatedNavBar from "../components/PopulatedNavBar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
