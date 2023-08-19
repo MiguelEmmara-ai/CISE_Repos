9cc287636fce425a77444f1329eebc928212c09b
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-08-19 22:49:09 +1200

    Refactor BookCard and ShowBookList components

--- Week 3/MERN/mern_book_app_client/src/components/ShowBookList.js
+++ Week 3/MERN/mern_book_app_client/src/components/ShowBookList.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "../App.css";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import BookCard from "./BookCard";
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BookCard from './BookCard';
 
 function ShowBookList() {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://mern-book-app-api.vercel.app/api/books")
+      .get('https://mern-book-app-api.vercel.app/api/books')
       .then((res) => {
         setBooks(res.data);
       })
       .catch((err) => {
-        console.log("Error from ShowBookList");
+        console.log('Error from ShowBookList');
       });
   }, []);
 
   const bookList =
     books.length === 0
-      ? "there is no book record!"
+      ? 'there is no book record!'
       : books.map((book, k) => <BookCard book={book} key={k} />);
 
   return (
@@ -29,7 +29,11 @@ function ShowBookList() {
         <div className='row'>
           <div className='col-md-12'>
             <br />
-            <h2 className='display-4 text-center'>Books List</h2>
+            <h2 className='display-4 text-center'>
+              <Link to={'/'} style={{ color: 'inherit' }}>
+                Books List
+              </Link>
+            </h2>
           </div>
 
           <div className='col-md-11'>
